0d45e885fb07d97630431f3fc7e6db78997cef2c
Mykyta <hromov.mykyta@gmail.com>
2023-09-22 21:53:47 +0200

    Flask Back-end

---
+++ config.py
@@ -0,0 +1,17 @@
+import redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class ApplicationConfig:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = r'sqlite:///./db.sqlite'
+
+    SESSION_TYPE = 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
