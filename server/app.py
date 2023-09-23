69dc2f9a92f1cfb7767e3219dd69a61070aaca09
mishutkaMD <145787332+mishutkaMD@users.noreply.github.com>
2023-09-23 03:36:16 +0200

    Added API to store login information

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_session import Session
 from config import ApplicationConfig
 from models import db, User
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
 server_session = Session(app)
 db.init_app(app)
 
