3575e3b093522b734dea1ebf871c010e1d7e5d0d
elmalving <sjjsjdjd.sksksj@gmail.com>
2024-06-02 00:24:13 +0200

    Setup prettier

--- front-end/src/pages/NotFound.jsx
+++ front-end/src/pages/NotFound.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className='content'>
+        <div className="content">
             <div className="welcome-text align-middle">Page Not Found</div>
         </div>
     );
-}
+};
 
 export default NotFound;
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
-import httpClient from "../httpClient";
+import React, { useState } from 'react';
+import httpClient from '../httpClient';
 import { Link } from 'react-router-dom';
 import '../css/register.css';
 
 const Register = () => {
-    const [firstName, setfirstName] = useState("");
-    const [lastName, setlastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [degree, setDegree] = useState("");
+    const [firstName, setfirstName] = useState('');
+    const [lastName, setlastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [degree, setDegree] = useState('');
     const [termsChecked, setTerms] = useState(false);
 
     const signUpUser = async () => {
-        if (!termsChecked || email == '' || password == '' || firstName == '' || lastName == '' || degree == '')
-        {
+        if (
+            !termsChecked ||
+            email == '' ||
+            password == '' ||
+            firstName == '' ||
+            lastName == '' ||
+            degree == ''
+        ) {
             return;
         }
         try {
@@ -22,12 +28,11 @@ const Register = () => {
                 password,
                 firstName,
                 lastName,
-                degree
+                degree,
             });
 
             window.location.href = '/';
-        }
-        catch (e) {
+        } catch (e) {
             if (e.response.status === 409) {
                 alert(e.response.data.error);
             }
@@ -39,45 +44,62 @@ const Register = () => {
             <div className="content">
                 <div className="studymate-container">
                     <div className="align-left">
-                        <img src='./brand.svg' className="logo"/>
+                        <img src="./brand.svg" className="logo" />
                     </div>
                     <div className="align-right align-middle">
-                        <Link className="font-gradient" to={'/login'}>Log In</Link>
+                        <Link className="font-gradient" to={'/login'}>
+                            Log In
+                        </Link>
                     </div>
                 </div>
                 <div className="column-container">
                     <div className="login-content align-center">
                         <div className="row-gap">
                             <div className="login-welcome">
-                                Welcome aboard! Let's start your educational journey together.
+                                Welcome aboard! Let's start your educational
+                                journey together.
                             </div>
                         </div>
                         <form className="login-form">
                             <div className="row-gap">
                                 <div className="column-gap">
                                     <div className="column-container">
-                                        <label className="column-label align-left" htmlFor="firstName">First Name</label>
+                                        <label
+                                            className="column-label align-left"
+                                            htmlFor="firstName"
+                                        >
+                                            First Name
+                                        </label>
                                         <div className="input-container">
                                             <input
                                                 id="firstName"
                                                 placeholder="First Name"
                                                 type="text"
                                                 value={firstName}
-                                                onChange={(e) => setfirstName(e.target.value)}
+                                                onChange={(e) =>
+                                                    setfirstName(e.target.value)
+                                                }
                                                 className="input input-placeholder"
                                                 required
                                             />
                                         </div>
                                     </div>
                                     <div className="column-container">
-                                        <label className="column-label align-left" htmlFor="lastName">Last Name</label>
+                                        <label
+                                            className="column-label align-left"
+                                            htmlFor="lastName"
+                                        >
+                                            Last Name
+                                        </label>
                                         <div className="input-container">
                                             <input
                                                 id="lastName"
                                                 placeholder="Last Name"
                                                 type="text"
                                                 value={lastName}
-                                                onChange={(e) => setlastName(e.target.value)}
+                                                onChange={(e) =>
+                                                    setlastName(e.target.value)
+                                                }
                                                 className="input input-placeholder"
                                                 required
                                             />
@@ -88,28 +110,42 @@ const Register = () => {
                             <div className="row-gap">
                                 <div className="column-gap">
                                     <div className="column-container">
-                                        <label className="column-label align-left" htmlFor="email">Email</label>
+                                        <label
+                                            className="column-label align-left"
+                                            htmlFor="email"
+                                        >
+                                            Email
+                                        </label>
                                         <div className="input-container">
                                             <input
                                                 id="email"
                                                 placeholder="Email"
                                                 type="email"
                                                 value={email}
-                                                onChange={(e) => setEmail(e.target.value)}
+                                                onChange={(e) =>
+                                                    setEmail(e.target.value)
+                                                }
                                                 className="input input-placeholder"
                                                 required
                                             />
                                         </div>
                                     </div>
                                     <div className="column-container">
-                                        <label className="column-label align-left" htmlFor="password">Password</label>
+                                        <label
+                                            className="column-label align-left"
+                                            htmlFor="password"
+                                        >
+                                            Password
+                                        </label>
                                         <div className="input-container">
                                             <input
                                                 id="password"
                                                 placeholder="Password"
                                                 type="password"
                                                 value={password}
-                                                onChange={(e) => setPassword(e.target.value)}
+                                                onChange={(e) =>
+                                                    setPassword(e.target.value)
+                                                }
                                                 className="input input-placeholder"
                                                 required
                                             />
@@ -119,41 +155,69 @@ const Register = () => {
                             </div>
                             <div className="row-gap">
                                 <div className="column-container">
-                                    <label className="column-label align-left" htmlFor="degree">Current Degree</label>
+                                    <label
+                                        className="column-label align-left"
+                                        htmlFor="degree"
+                                    >
+                                        Current Degree
+                                    </label>
                                     <div className="input-container">
                                         <select
                                             id="degree"
                                             value={degree}
-                                            onChange={(e) => setDegree(e.target.value)}
+                                            onChange={(e) =>
+                                                setDegree(e.target.value)
+                                            }
                                             className="select"
                                             required
-                                            >
-                                            <option defaultValue={''} disabled hidden></option>
-                                            <option value="Elementary">Elementary education</option>
-                                            <option value="Upper">Upper secondary education</option>
-                                            <option value="Tertiary">Tertiary or university education</option>
+                                        >
+                                            <option
+                                                defaultValue={''}
+                                                disabled
+                                                hidden
+                                            ></option>
+                                            <option value="Elementary">
+                                                Elementary education
+                                            </option>
+                                            <option value="Upper">
+                                                Upper secondary education
+                                            </option>
+                                            <option value="Tertiary">
+                                                Tertiary or university education
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
                             </div>
                             <div className="input-misc">
                                 <div className="remember-container">
-                                    <input 
-                                    type="checkbox" 
-                                    id="remember-checkbox" 
-                                    onChange={() => setTerms(!termsChecked)}
-                                    checked={termsChecked}
-                                    required
+                                    <input
+                                        type="checkbox"
+                                        id="remember-checkbox"
+                                        onChange={() => setTerms(!termsChecked)}
+                                        checked={termsChecked}
+                                        required
                                     />
-                                    <label className="remember-label" htmlFor="remember-checkbox">
+                                    <label
+                                        className="remember-label"
+                                        htmlFor="remember-checkbox"
+                                    >
                                         I agree with&nbsp;
-                                        <a className="font-gradient" target="_blank" href="https://github.com/elmalving/Hackathon-2023/blob/main/LICENSE">
+                                        <a
+                                            className="font-gradient"
+                                            target="_blank"
+                                            href="https://github.com/elmalving/Hackathon-2023/blob/main/LICENSE"
+                                        >
                                             Terms and conditions
                                         </a>
                                     </label>
                                 </div>
                             </div>
-                            <button className='submit-button submit-label' type='button' onClick={() => signUpUser()}>
+                            <button
+                                className="submit-button submit-label"
+                                type="button"
+                                onClick={() => signUpUser()}
+                            >
                                 Create an account
                             </button>
                         </form>
@@ -163,6 +227,6 @@ const Register = () => {
             <div className="bg-image illustration"></div>
         </div>
     );
-}
+};
 
 export default Register;
